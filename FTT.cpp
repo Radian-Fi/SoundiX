c882f14059f3bd91df062cf9a65edae88649b7d8
Radian-Fi <adrian.filcik@gmail.com>
2017-12-19 02:48:40 +0100

    New files added, improved version of FTT coming soon.

---
+++ FTT.cpp
@@ -0,0 +1,8 @@
+﻿#define _USE_MATH_DEFINES
+#include <math.h>
+
+for (int i = 0; i < 2048; i++) {
+	/* Twice the size of the buffer lenghth */
+    double multiplier = 0.5 * (1 - cos(2*M_PIl*i/2047));
+    dataOut[i] = multiplier * dataIn[i];
+}
