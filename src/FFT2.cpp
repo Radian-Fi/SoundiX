5e38a80a301658427cdc9d2d65b3abefb6b69bc7
Test Computer <adrian.filcik@gmail.com>
2018-02-26 01:50:22 +0100

    New version 0.1.3a, added Hanning window, added pre-MIDI output file notes.out

--- src/FFT2.cpp
+++ src/FFT2.cpp
@@ -1,134 +1,228 @@
 #include <iostream>
 #include <complex>
+#include <stdio.h>
 #include <stdlib.h>
-#define MAX 200
 #include <sndfile.h>
-int *buf;
-int num,c;
+#include <fstream>
+#include <lsndx.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <limits>
+#include <vector>
+#include <iterator>
 
 using namespace std;
 
+//int BUFFER_LEN; //number of samples (in window)
+int sr, num;
+int MAX;
+double volumax = numeric_limits<double>::min();
+double volumin = numeric_limits<double>::max();
 
 int log2(int N)    /*function to calculate the log2(.) of int numbers*/
 {
-  int k = N, i = 0;
-  while(k) {
-    k >>= 1;
-    i++;
-  }
-  return i - 1;
+	int k = N, i = 0;
+	while(k) {
+		k >>= 1;
+		i++;
+	}
+	return i - 1;
 }
 
 int check(int n)    //checking if the number of element is a power of 2
 {
-  return n > 0 && (n & (n - 1)) == 0;
+	return n > 0 && (n & (n - 1)) == 0;
 }
 
 int reverse(int N, int n)    //calculating revers number
 {
-  int j, p = 0;
-  for(j = 1; j <= log2(N); j++) {
-    if(n & (1 << (log2(N) - j)))
-      p |= 1 << (j - 1);
-  }
-  return p;
+	  int j, p = 0;
+	  for(j = 1; j <= log2(N); j++) {
+		if(n & (1 << (log2(N) - j)))
+			p |= 1 << (j - 1);
+	  }
+	  return p;
 }
 
-void ordina(complex<double>* f1, int N) //using the reverse order in the array
+void order(complex<double>* f1, int N) //using the reverse order in the array
 {
-  complex<double> f2[MAX];
-  for(int i = 0; i < N; i++)
-    f2[i] = f1[reverse(N, i)];
-  for(int j = 0; j < N; j++)
-    f1[j] = f2[j];
+	complex<double> f2[N];
+	for(int i = 0; i < N; i++) {
+		f2[i] = f1[reverse(N, i)];
+	}
+	for(int j = 0; j < N; j++) {
+		f1[j] = f2[j];
+	}
 }
 
 void transform(complex<double>* f, int N) //
 {
-  ordina(f, N);    //first: reverse order
-  complex<double> *W;
-  W = (complex<double> *)malloc(N / 2 * sizeof(complex<double>));
-  W[1] = polar(1., -2. * M_PI / N);
-  W[0] = 1;
-  for(int i = 2; i < N / 2; i++)
-    W[i] = pow(W[1], i);
-  int n = 1;
-  int a = N / 2;
-  for(int j = 0; j < log2(N); j++) {
-    for(int i = 0; i < N; i++) {
-      if(!(i & n)) {
-        complex<double> temp = f[i];
-        complex<double> Temp = W[(i * a) % (n * a)] * f[i + n];
-        f[i] = temp + Temp;
-        f[i + n] = temp - Temp;
-      }
-    }
-    n *= 2;
-    a = a / 2;
-  }
+	order(f, N);    //first: reverse order
+	complex<double> *W;
+	W = (complex<double> *)malloc(N / 2 * sizeof(complex<double>));
+	W[1] = polar(1., -2. * M_PI / N);
+	W[0] = 1;
+	for(int i = 2; i < N / 2; i++)
+	W[i] = pow(W[1], i);
+	int n = 1;
+	int a = N / 2;
+	for(int j = 0; j < log2(N); j++) {
+		for(int i = 0; i < N; i++) {
+			if(!(i & n)) {
+				complex<double> temp = f[i];
+				complex<double> Temp = W[(i * a) % (n * a)] * f[i + n];
+				f[i] = temp + Temp;
+				f[i + n] = temp - Temp;
+			}
+		}
+		n *= 2;
+		a = a / 2;
+	}
+}
+
+void decomplex(complex<double>* f, int N, double result[][2])
+{
+	double df = (double) sr / (N*2);
+	for(int i = 0; i < N; i++)
+	{
+		result[i][0] = abs(f[i]);
+		result[i][1] = df*i;
+	}
+}
+
+int compare(const void* a, const void* b) {
+	double* arr1 = (double*)a;
+	double* arr2 = (double*)b;
+	if (arr1[0] > arr2[0]) return 1;
+	else if (arr1[0] < arr2[0]) return -1; //else not needed here, just for better structure
+	else return 0;
+}
+
+int filter(double res[][2], int N)
+{
+	qsort(res, N, 2*sizeof(double), compare);
+	double last = numeric_limits<double>::max();
+	int index = 0;
+	for (int i = 0; i < N; ++i)
+	{
+		if (res[i][1] < last) //also could be if (res[i][1] = 0)
+		{
+			last = res[i][1];
+			index = i;
+		}
+	}
+	return index;
+}
+
+void vminmax(double res[][2], int N)
+{
+	if (res[0][0] < volumin)
+		volumin = res[0][0];
+	if (res[N-1][0] > volumax and res[N-1][1] != 0)
+		volumax = res[N-1][0];
+}
+
+double volume(double x)
+{
+	x = x-volumin;
+	x = x/(volumax-volumin)*127;
+	return x;
+}
+
+int freq(double y)
+{
+	y = round(69.0 + 12*log2(y/440));
+	return y;
+}
+
+void hann (complex<double>* dataIn, int N)
+{
+	for (int i = 0; i < N; i++)
+	{
+	double multiplier = 0.5 * (1 - cos((2*M_PIl*i)/(N-1)));
+	dataIn[i] = multiplier * dataIn[i];
+	}
 }
 
 void FFT(complex<double>* f, int N, double d)
 {
-  transform(f, N);
-  for(int i = 0; i < N; i++)
-    f[i] *= d; //multiplying by step
-}
-
-void decode()
-{
-    SNDFILE *sf;
-    SF_INFO info;
-    //int num_channels;
-    int num_items;
-    int f,sr;
-    char fname[260];
-
-    
-    /* Open the WAV file. */
-    info.format = 0;
-    cout << "Select the file path: ";
-    cin.getline(fname, sizeof fname);
-    sf = sf_open(fname,SFM_READ,&info);
-    if (sf == NULL) {
-        printf("Failed to open the file.\n");
-        exit(-1);
-        }
-    /* Print some of the info, and figure out how much data to read. */
-    f = info.frames;
-    sr = info.samplerate;
-    c = info.channels;
-    printf("frames=%d\n",f);
-    printf("samplerate=%d\n",sr);
-    printf("channels=%d\n",c);
-    num_items = f*c;
-    printf("num_items=%d\n",num_items);
-    /* Allocate space for the data to be read, then read it. */
-    buf = (int *) malloc(num_items*sizeof(int));
-    num = sf_read_int(sf,buf,num_items);
-    sf_close(sf);
-    printf("Read %d items\n",num);
+	transform(f, N);
+	for(int i = 0; i < N; i++)
+	f[i] *= d; //multiplying by step
 }
 
 int main()
 {
-  decode();
-  int n;
-  do {
-    cout << "specify array dimension (MUST be power of 2)" << endl;
-    cin >> n;
-  } while(!check(n));
-  double d;
-  cout << "specify sampling step" << endl; //just write 1 in order to have the same results of matlab fft(.)
-  cin >> d;
-  complex<double> vec[MAX];
-  cout << "specify the array" << endl;
-  for(int i = 0; i < n; i++) {
-    vec[i] = buf[i];
-  }
-  FFT(vec, n, d);
-  cout << "...printing the FFT of the array specified" << endl;
-  for(int j = 0; j < n; j++)
-    cout << vec[j] << endl;
-  return 0;
+	double d = 1; //sampeling step
+	decode();
+	int MAX = pow(2,floor(log2(sr)));
+	complex<double> vec[MAX];
+	double result[MAX/2][2];
+	int i,j;
+	int m = 0;
+	int o = 0;
+	complex<double> a[num];
+	//vector<vector<vector<double>>> notes;
+	fstream myfile("filedata.out", ios_base::in);
+	for (int j = 1; j < num+1; ++j)
+	{
+		myfile >> a[j];
+		cout << "Reading audio data: " << (int)(j*100/num) << "%\r";
+		cout.flush();
+	}
+	myfile.close();
+	cout << "Reading audio data: 100%" << endl;
+	fstream myfile2("filedata.out", ios_base::out);
+	for (int i = 0; floor(i*MAX/50) < num; ++i)
+        {
+		fill(vec, vec+MAX, 0);
+		for (int j = 0; j < MAX; ++j)
+		{
+			vec[j] = a[(int)floor(i*MAX/50)+j];
+		}
+		cout << "Computing FFT: " << (int)(m/(num/floor(MAX/0.5))) << "%\r";
+		cout.flush();
+		hann(vec, MAX);
+		FFT(vec, MAX, d);
+		decomplex(vec, MAX/2, result);
+		int n = filter(result, MAX/2);
+		vminmax(result, MAX/2);
+		//cout << "...printing the FFT for the ";
+		//cout << i*MAX << " to " << (i+1)*MAX-1 << "samples." << endl;
+		/*for(int j = n; j < MAX/2; ++j)
+			cout << result[j][1] << endl;*/
+		for (int j = n; j < MAX/2; ++j)
+		{
+			/*notes.push_back(vector<vector<double>>());
+			notes[i].push_back(vector<double>());
+			notes[i][j].push_back(result[j][0]);
+			notes[i][j].push_back(result[j][1]);*/
+			myfile2 << m << endl;
+			myfile2 << result[j][0] << endl;
+			myfile2 << result[j][1] << endl;
+			o+=1;
+		}
+		m+=1;
+        }
+	myfile2.close();
+	cout << "Computing FFT: 100%" << endl;
+	fstream myfile3("filedata.out", ios_base::in);
+	fstream myfile4("notes.out", ios_base::out);
+	int t;
+	double x, y;
+	for (int j = 0; j < o; ++j)
+	{
+		myfile3 >> t >> x >> y;
+		myfile4 << t << endl;
+		myfile4 << volume(x) << endl;
+		myfile4 << freq(y) << endl;
+		cout << "Writing out (to notes.out): " << (int)(j*100/o) << "%\r";
+		cout.flush();
+	}
+	myfile3.close();
+	myfile4.close();
+	cout << "Writing out (to notes.out): 100%" << endl;
+	//copy(begin(notes), end(notes), ostream_iterator<double>(cout, " "));
+	cout << "Done." << endl;
+	return 0;
 }
---
+++ src/Open2.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sndfile.h>
+#include <math.h>
+#include <tuple>
+
+using namespace std;
+
+extern int sr, num;
+
+int decode()
+{
+    SNDFILE *sf;
+    SF_INFO info;
+    //int num_channels;
+    //int sr, num;
+    int num_items;
+    int f,c;
+    int *buf;
+    FILE *out;
+    char fname[260];
+
+    
+    /* Open the WAV file. */
+    info.format = 0;
+    cout << "Select the file path: ";
+    cin.getline(fname, sizeof fname);
+    sf = sf_open(fname,SFM_READ,&info);
+    if (sf == NULL) {
+        printf("Failed to open the file.\n");
+        exit(-1);
+        }
+    /* Print some of the info, and figure out how much data to read. */
+    f = info.frames;
+    sr = info.samplerate;
+    c = info.channels;
+    printf("Frames=				%d\n",f);
+    printf("Samplerate=			%dHz\n",sr);
+    printf("Channels=			%d\n",c);
+    num_items = f*c;
+    printf("Total number of samples=	%d\n",num_items);
+    /* Allocate space for the data to be read, then read it. */
+    buf = (int *) malloc(num_items*sizeof(int));
+    num = sf_read_int(sf,buf,num_items);
+    sf_close(sf);
+    printf("Read %d samples\n",num);
+    /* Write the data to filedata.out. */
+    out = fopen("filedata.out","w");
+    for (int i = 0; i < num; i += 1)
+        {
+        for (int j = 0; j < 1; ++j)
+            fprintf(out,"%d ",buf[i+j]);
+        fprintf(out,"\n");
+        }
+    fclose(out);
+    return 0;
+}
+
+/*int main() {
+    decode();
+    return 0;
+}*/
