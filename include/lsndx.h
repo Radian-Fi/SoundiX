4b2ed158c83699b5989160681ed72a240f34452a
Radian-Fi <adrian.filcik@gmail.com>
2018-03-05 02:24:58 +0100

    freq2midi updated, minor changes in FFT2.

--- include/lsndx.h
+++ include/lsndx.h
@@ -1,1 +1,4 @@
 int decode(void);
+void d2b(fstream& myfile, int data, int bit);
+int start(int format, int tracks, int division);
+void noteOn(fstream& myfile, int channel, int note, int velocity);
