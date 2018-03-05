5c8c524cc8baac47b2379afea90e7f89eccf36ad
Radian-Fi <adrian.filcik@gmail.com>
2018-03-05 02:31:11 +0100

    Updated lsndx.h

--- include/lsndx.h
+++ include/lsndx.h
@@ -2,3 +2,5 @@ int decode(void);
 void d2b(fstream& myfile, int data, int bit);
 int start(int format, int tracks, int division);
 void noteOn(fstream& myfile, int channel, int note, int velocity);
+void track(fstream& myfile, long long length);
+void deltaTime(fstream& myfile, long long time);
