a06592cf49c2a6f61c4b3b129916f7304f0c3198
Radian-Fi <adrian.filcik@gmail.com>
2018-03-13 04:51:57 +0100

    Minor update.

--- src/Open2.cpp
+++ src/Open2.cpp
@@ -26,6 +26,42 @@ void info(char fname[260], int f, int sr, int c, int num_items)
     num_items = f*c;
 }
 
+void decodeMp3()
+{
+    mpg123_init();
+
+    int err;
+    mpg123_handle *mh = mpg123_new(NULL, &err);
+    unsigned char *buffer;
+    size_t buffer_size;
+    size_t done;
+
+    int channels, encoding;
+    long rate;
+    buffer_size = mpg123_outblock(mh);
+    buffer = (unsigned char*)malloc(buffer_size * sizeof(unsigned char));
+
+    mpg123_open(mh, "/home/abbas/Desktop/nastaran.mp3");
+    mpg123_getformat(mh, &rate, &channels, &encoding);
+
+    std::ofstream out("res.txt");
+    unsigned int counter = 0;
+
+    for (int totalBtyes = 0; mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK; ) {
+        short* tst = reinterpret_cast<short*>(buffer);
+        for (auto i = 0; i < buffer_size / 2; i++) {
+            out<< counter + i<<"\t"<< tst[i] << "\n";
+        }
+        counter += buffer_size/2;
+        totalBtyes += done;
+    }
+    out.close();
+    free(buffer);
+    mpg123_close(mh);
+    mpg123_delete(mh);
+    mpg123_exit();
+}
+
 int decode()
 {
     SNDFILE *sf;
