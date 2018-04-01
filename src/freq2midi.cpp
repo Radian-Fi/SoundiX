4824dcad2b7a040778977cbb4672c9e2ce0d08af
Radian-Fi <34257022+Radian-Fi@users.noreply.github.com>
2018-04-01 07:23:43 +0200

    printf replaced by cout

--- src/freq2midi.cpp
+++ src/freq2midi.cpp
@@ -81,15 +81,17 @@ long getFileSize(string filename)
     return rc == 0 ? stat_buf.st_size : -1;
 }
 
+/*
 void __attribute__ ((constructor)) initLibrary(void) {
  //
  // Function that is called when the library is loaded
  //
-    printf("Library is initialized\n");
+    cout << "Library is initialized." << endl;
 }
 void __attribute__ ((destructor)) cleanUpLibrary(void) {
  //
  // Function that is called when the library is »closed«.
  //
-    printf("Library is exited\n"); 
+    cout << "Library is exited." << endl;
 }
+*/
