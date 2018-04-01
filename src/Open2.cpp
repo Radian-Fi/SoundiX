7640f48ddc9160f91b5378e200b1fcaeb7a2f112
Radian-Fi <34257022+Radian-Fi@users.noreply.github.com>
2018-04-01 07:25:19 +0200

    Changed formatting, few unnecessary variables deleted, whole mp3 support part removed, printf replaced by cout.

--- src/Open2.cpp
+++ src/Open2.cpp
@@ -1,11 +1,11 @@
+#include <cmath>
 #include <iostream>
-#include <stdio.h>
 #include <stdlib.h>
 #include <sndfile.h>
-#include <math.h>
 #include <complex>
 
 using namespace std;
+
 
 void info(char fname[260], int* f, int* sr, int* c, int* num_items)
 {
@@ -15,8 +15,8 @@ void info(char fname[260], int* f, int* sr, int* c, int* num_items)
     sf = sf_open(fname,SFM_READ,&info);
     if (sf == NULL)
     {
-        printf("Failed to open the file.\n");
-        cout << "the file was " << fname << endl;
+        cout << "Failed to open the file. << endl;
+        cout << "The file was " << fname << endl;
         exit(-1);
 	}
     *f = info.frames;
@@ -26,54 +26,14 @@ void info(char fname[260], int* f, int* sr, int* c, int* num_items)
     sf_close(sf);
 }
 
-/*
-void decodeMp3(char fname[260], complex<double> a[])
-{
-    mpg123_init();
-
-    int err;
-    mpg123_handle *mh = mpg123_new(NULL, &err);
-    unsigned char *buffer;
-    size_t buffer_size;
-    size_t done;
-
-    int channels, encoding;
-    long rate;
-    buffer_size = mpg123_outblock(mh);
-    buffer = (unsigned char*)malloc(buffer_size * sizeof(unsigned char));
-
-    mpg123_open(mh, fname);
-    mpg123_getformat(mh, &rate, &channels, &encoding);
-
-    unsigned int counter = 0;
-
-    for (int totalBytes = 0; mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK; )
-    {
-        short* tst = reinterpret_cast<short*>(buffer);
-        for (auto i = 0; i < buffer_size / 2; i++)
-        {
-            a[totalBytes+i] = tst[i];
-        }
-        counter += buffer_size/2;
-        totalBytes += done;
-    }
-    free(buffer);
-    mpg123_close(mh);
-    mpg123_delete(mh);
-    mpg123_exit();
-}
-*/
-
 void decode(char fname[260], complex<double> a[])
 {
     SNDFILE *sf;
     SF_INFO info;
-    //int num_channels;
     int sr, num;
     int num_items;
     int f,c;
     int *buf;
-    //FILE *out;
 
     
     /* Open the WAV file. */
@@ -83,16 +43,16 @@ void decode(char fname[260], complex<double> a[])
     f = info.frames;
     sr = info.samplerate;
     c = info.channels;
-    printf("Frames=				%d\n",f);
-    printf("Samplerate=			%dHz\n",sr);
-    printf("Channels=			%d\n",c);
+    cout << "Frames=				" << f << endl;
+    cout << "Samplerate=			" << sr << endl;
+    cout << "Channels=				" << c << endl;
     num_items = f*c;
-    printf("Total number of samples=	%d\n",num_items);
+    cout << "Total number of samples=	" << num_items << endl;
     /* Allocate space for the data to be read, then read it. */
     buf = (int *) malloc(num_items*sizeof(int));
     num = sf_read_int(sf,buf,num_items);
     sf_close(sf);
-    printf("Read %d samples\n",num);
+    cout << "Read " << num << " samples" << endl;
     /* Write the data to filedata.out. */
 	for (int i = 0; i < num; i += 1)
     {
@@ -102,8 +62,3 @@ void decode(char fname[260], complex<double> a[])
 		}
     }
 }
-
-/*int main() {
-    decode();
-    return 0;
-}*/
